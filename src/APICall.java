cbfa6d38769a96a214b5eef8bec79e39255ce707
rguan1 <guan.ryan@gmail.com>
2018-04-23 23:00:09 -0400

    second commit

--- src/APICall.java
+++ src/APICall.java
@@ -1,4 +1,5 @@
 import com.careerjet.webservice.api.Client;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +18,24 @@ public class APICall {
         arguments.put("user_agent", "client");
         arguments.put("url", "https://www.google.com/");
         JSONObject output = (JSONObject) myClient.search(arguments);
-        System.out.println(output.toString());
+
+        if (output.get("type").equals("JOBS")) {
+            JSONArray jobReturn = (JSONArray) output.get("jobs");
+            for (Object obj : jobReturn) {
+                JSONObject job = (JSONObject) obj;
+                System.out.println("URL         :" + job.get("url"));
+                System.out.println("TITLE       :" + job.get("title"));
+                System.out.println("COMPANY     :" + job.get("company"));
+                System.out.println("SALARY      :" + job.get("salary"));
+                System.out.println("DATE        :" + job.get("date"));
+                System.out.println("DESCRIPTION :" + job.get("description"));
+                System.out.println("SITE        :" + job.get("site"));
+                System.out.println("LOCATIONS   :" + job.get("locations"));
+                System.out.println("\n");
+            }
+        }
+
+        //System.out.println(output.toString());
     }
 
 }
